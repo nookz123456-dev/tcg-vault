614312143a10a822e65924783239f5ebfec3e769
nookz123456-dev <nookz123456-dev@users.noreply.github.com>
2026-04-18 02:38:52 +0700

    Clean up temp scripts

--- check_lines.py
+++
@@ -1,4 +0,0 @@
-data = open(r'C:\Users\suwij\.openclaw\workspace\tcg-vault\src\app\discussions\page.tsx', encoding='utf-8').readlines()
-for i, line in enumerate(data):
-    if 193 <= i <= 200:
-        print(f'{i+1}: {repr(line.rstrip())}')
