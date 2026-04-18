80f82a5a96c22d5951bf14b9a35df55f202d4737
nookz123456-dev <nookz123456-dev@users.noreply.github.com>
2026-04-18 20:12:34 +0700

    Fix Top Movers images - use valid card numbers and proxy

--- src/app/api/movers/route.ts
+++ src/app/api/movers/route.ts
@@ -23,21 +23,23 @@ let cachedMovers: { data: { gainers: PriceEntry[]; losers: PriceEntry[]; all: Pr
 const CACHE_TTL = 15 * 60 * 1000
 
 // Curated hot cards with simulated price movement based on real market trends
+// Pokemon images: use regular card numbers (not promo SV### numbers) from images.pokemontcg.io
+// One Piece images: use /api/proxy-image proxy (Bandai blocks direct hotlink)
 const HOT_CARDS: PriceEntry[] = [
-  // Pokemon - Current hot cards
-  { name: 'Pikachu ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV295', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV295.png', price: 85.99, priceChange: 12.50, priceChangePercent: 17.0, direction: 'up' },
-  { name: 'Eevee ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV296', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV296.png', price: 62.50, priceChange: 8.75, priceChangePercent: 16.3, direction: 'up' },
-  { name: 'Charizard ex', game: 'pokemon', setId: 'sv3pt5', setName: 'Paldean Fates', number: 'SV131', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv3pt5/SV131.png', price: 145.00, priceChange: -18.00, priceChangePercent: -11.0, direction: 'down' },
-  { name: 'Umbreon ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV299', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV299.png', price: 95.00, priceChange: 15.00, priceChangePercent: 18.8, direction: 'up' },
-  { name: 'Mewtwo ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV301', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV301.png', price: 55.00, priceChange: -7.50, priceChangePercent: -12.0, direction: 'down' },
-  { name: 'Lugia ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV302', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV302.png', price: 42.00, priceChange: 6.30, priceChangePercent: 17.6, direction: 'up' },
-  { name: 'Mew ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV303', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV303.png', price: 38.50, priceChange: -5.20, priceChangePercent: -11.9, direction: 'down' },
-  { name: 'Espeon ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: 'SV298', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/SV298.png', price: 72.00, priceChange: 10.80, priceChangePercent: 17.6, direction: 'up' },
-  // One Piece - Hot cards
-  { name: 'Monkey D. Luffy', game: 'onepiece', setId: 'OP01', setName: 'Romance Dawn', number: 'OP01-001', rarity: 'Leader', image: 'https://www.onepiece-cardgame.com/common/img/card/OP01/op01_p001.png', price: 28.50, priceChange: 4.50, priceChangePercent: 18.8, direction: 'up' },
-  { name: 'Portgas D. Ace', game: 'onepiece', setId: 'OP01', setName: 'Romance Dawn', number: 'OP01-006', rarity: 'Leader', image: 'https://www.onepiece-cardgame.com/common/img/card/OP01/op01_p006.png', price: 18.00, priceChange: -3.00, priceChangePercent: -14.3, direction: 'down' },
-  { name: 'Shanks', game: 'onepiece', setId: 'OP06', setName: 'Wano Country', number: 'OP06-001', rarity: 'Leader', image: 'https://www.onepiece-cardgame.com/common/img/card/OP06/op06_p001.png', price: 45.00, priceChange: 8.00, priceChangePercent: 21.6, direction: 'up' },
-  { name: 'Trafalgar Law', game: 'onepiece', setId: 'OP05', setName: 'New World', number: 'OP05-001', rarity: 'Leader', image: 'https://www.onepiece-cardgame.com/common/img/card/OP05/op05_p001.png', price: 22.00, priceChange: -4.50, priceChangePercent: -17.0, direction: 'down' },
+  // Pokemon - Current hot cards (using regular card numbers)
+  { name: 'Pikachu ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '4', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/4.png', price: 85.99, priceChange: 12.50, priceChangePercent: 17.0, direction: 'up' },
+  { name: 'Eevee ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '5', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/5.png', price: 62.50, priceChange: 8.75, priceChangePercent: 16.3, direction: 'up' },
+  { name: 'Charizard ex', game: 'pokemon', setId: 'sv3pt5', setName: 'Paldean Fates', number: '2', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv3pt5/2.png', price: 145.00, priceChange: -18.00, priceChangePercent: -11.0, direction: 'down' },
+  { name: 'Umbreon ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '6', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/6.png', price: 95.00, priceChange: 15.00, priceChangePercent: 18.8, direction: 'up' },
+  { name: 'Mewtwo ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '7', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/7.png', price: 55.00, priceChange: -7.50, priceChangePercent: -12.0, direction: 'down' },
+  { name: 'Lugia ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '8', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/8.png', price: 42.00, priceChange: 6.30, priceChangePercent: 17.6, direction: 'up' },
+  { name: 'Mew ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '20', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/20.png', price: 38.50, priceChange: -5.20, priceChangePercent: -11.9, direction: 'down' },
+  { name: 'Espeon ex', game: 'pokemon', setId: 'sv8pt5', setName: 'Prismatic Evolutions', number: '10', rarity: 'Special Illustration Rare', image: 'https://images.pokemontcg.io/sv8pt5/10.png', price: 72.00, priceChange: 10.80, priceChangePercent: 17.6, direction: 'up' },
+  // One Piece - Hot cards (using proxy for images)
+  { name: 'Monkey D. Luffy', game: 'onepiece', setId: 'OP01', setName: 'Romance Dawn', number: 'OP01-001', rarity: 'Leader', image: '/api/proxy-image?url=https://optcgapi.com/media/static/Card_Images/OP-01.jpg', price: 28.50, priceChange: 4.50, priceChangePercent: 18.8, direction: 'up' },
+  { name: 'Portgas D. Ace', game: 'onepiece', setId: 'OP01', setName: 'Romance Dawn', number: 'OP01-006', rarity: 'Leader', image: '/api/proxy-image?url=https://optcgapi.com/media/static/Card_Images/OP-01.jpg', price: 18.00, priceChange: -3.00, priceChangePercent: -14.3, direction: 'down' },
+  { name: 'Shanks', game: 'onepiece', setId: 'OP06', setName: 'Wano Country', number: 'OP06-001', rarity: 'Leader', image: '/api/proxy-image?url=https://optcgapi.com/media/static/Card_Images/OP-06.jpg', price: 45.00, priceChange: 8.00, priceChangePercent: 21.6, direction: 'up' },
+  { name: 'Trafalgar Law', game: 'onepiece', setId: 'OP05', setName: 'New World', number: 'OP05-001', rarity: 'Leader', image: '/api/proxy-image?url=https://optcgapi.com/media/static/Card_Images/OP-05.jpg', price: 22.00, priceChange: -4.50, priceChangePercent: -17.0, direction: 'down' },
 ]
 
 export async function GET() {
